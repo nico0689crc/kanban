bb7abbd60f2b75b55d03de6cd56db51c343c6d5a
Nicolas Fernandez <nico.06.89crc@gmail.com>
2023-12-28 17:25:11 +0800

    Seeding Database - fixes

--- server/src/database/seeders/20231228073147-add-users.js
+++ server/src/database/seeders/20231228073147-add-users.js
@@ -1,23 +1,38 @@
 'use strict';
 
 const { faker } = require("@faker-js/faker")
-
 /** @type {import('sequelize-cli').Migration} */
+
 module.exports = {
   async up ({ context: queryInterface }) {
-     await queryInterface.bulkInsert('users', [{
-      first_name: faker.person.firstName(),
-      last_name: faker.person.lastName(),
-      email: faker.internet.email(),
-      token: faker.string.alphanumeric(10),
-      confirmation_code: faker.string.numeric(5),
-      avatar: faker.internet.avatar(),
-      password: faker.string.alphanumeric(5),
-      role: faker.helpers.enumValue({
-        user: 'user', 
-        admin: 'admin'
-      }),
-     }], {});
+     await queryInterface.bulkInsert('users', [
+      {
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        email: faker.internet.email(),
+        token: faker.string.alphanumeric(10),
+        confirmation_code: faker.string.numeric(5),
+        avatar: faker.internet.avatar(),
+        password: faker.string.alphanumeric(5),
+        role: faker.helpers.enumValue({
+          user: 'user', 
+          admin: 'admin'
+        }),
+       },
+       {
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        email: faker.internet.email(),
+        token: faker.string.alphanumeric(10),
+        confirmation_code: faker.string.numeric(5),
+        avatar: faker.internet.avatar(),
+        password: faker.string.alphanumeric(5),
+        role: faker.helpers.enumValue({
+          user: 'user', 
+          admin: 'admin'
+        }),
+       }
+     ], {});
   },
 
   async down ({ context: queryInterface }) {
