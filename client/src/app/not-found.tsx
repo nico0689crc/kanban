e5255083c51c7d344cd73330cf1957a7972f1ff0
Nicolas Fernandez <nico.06.89crc@gmail.com>
2024-02-02 22:18:36 +0800

    Not found page

--- client/src/app/not-found.tsx
+++ client/src/app/not-found.tsx
@@ -1,14 +1,30 @@
-import { Box, Stack, Typography } from '@mui/material';
+'use client';
+
+import { Box, Button, Stack } from '@mui/material';
+import { RouterLink } from '@/routes/components';
+import { paths } from '@/routes/paths';
+import NotFoundView from '@/sections/error/not-found-view';
+import { useLocales } from '@/locales';
 
 export const metadata = {
   title: '404 Page Not Found!',
 };
 
 const NotFoundPage = () => {
+  const { t } = useLocales();
+
   return (
     <Box sx={{height: '100vh', width: '100vw'}}>
       <Stack height='100%' alignItems='center' justifyContent='center'>
-        <Typography variant='h3'>Not Found Page</Typography>
+        <NotFoundView
+            title={t('common.labels.not_found_title')}
+            subTitle={t('common.labels.not_found_subtitle')}
+            caButton={(
+              <Button component={RouterLink} href={paths.dashboard.root} size="large" color='primary' variant="contained">
+                {t('common.labels.not_found_cabutton')}
+              </Button>
+            )} 
+          />
       </Stack>
     </Box>
   )
